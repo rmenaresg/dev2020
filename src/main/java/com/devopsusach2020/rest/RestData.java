512c7e78467e20f15c479c608e3404a02244a213
Roberto <roberto.menaresg@gmail.com>
2020-10-25 18:53:53 -0300

    version 0.2

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -1,5 +1,8 @@
 package com.devopsusach2020.rest;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +20,14 @@ import com.google.gson.Gson;
 @RequestMapping(path = "/rest/mscovid")
 public class RestData {
 	
+	private final static Logger LOGGER = Logger.getLogger("devops.subnivel.Control");
+
+	
 	@GetMapping(path = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Pais getData(@RequestParam(name = "msg") String message){
+		
+		LOGGER.log(Level.INFO, "Proceso exitoso de prueba");
+		
 		Pais response = new Pais();
 		response.setMensaje("Mensaje Recibido: " + message);
 		return response;
@@ -29,17 +38,17 @@ public class RestData {
 	public @ResponseBody Pais getTotalPais(@RequestParam(name = "pais") String message){
 		RestTemplate restTemplate = new RestTemplate();
 	    ResponseEntity<String> call= restTemplate.getForEntity("https://api.covid19api.com/live/country/" + message ,String.class);
-	    System.out.println(call.getBody());
-		
+	    
+	    LOGGER.log(Level.INFO, "Consulta por pais");
+	    
 		Pais response = new Pais();
 		int confirmed = 0;
 		int death = 0;
 		int recovered = 0;
-		
 		Gson gson = new Gson();
         Pais[] estados = gson.fromJson(call.getBody().toLowerCase(), Pais[].class);
 
-        for (Pais estado : estados) {
+        for(Pais estado : estados) {
         	response.setDate(estado.getDate());
         	response.setActive(estado.getActive());
         	confirmed += estado.getConfirmed();
@@ -58,16 +67,15 @@ public class RestData {
 	
 
 	@GetMapping(path = "/estadoMundial", produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Mundial getTotalMundial(@RequestParam(name = "pais") String message){
+	public @ResponseBody Mundial getTotalMundial(){
+		
+		LOGGER.log(Level.INFO, "Consulta mundial");
+		
 		RestTemplate restTemplate = new RestTemplate();
 	    ResponseEntity<String> call= restTemplate.getForEntity("https://api.covid19api.com/world/total" ,String.class);
-	    System.out.println(call.getBody());
-	    
-		Mundial response = new Mundial();
-		
+	    Mundial response = new Mundial();
 		Gson gson = new Gson();
         Mundial estado = gson.fromJson(call.getBody().toLowerCase(), Mundial.class);
-
         response.setTotalConfirmed(estado.getTotalConfirmed());
         response.setTotalDeaths(estado.getTotalDeaths());
         response.setTotalRecovered(estado.getTotalRecovered());
